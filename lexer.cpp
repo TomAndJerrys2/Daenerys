167170d9f3761a32d44b53e9c2b228d4ec39ce3b
TomAndJerrys2 <kylev2005@hotmail.com>
2025-12-22 21:56:11 +0000

    Expression Trees for parsing

---
+++ lexer.cpp
@@ -0,0 +1,66 @@
+
+
+enum Token {
+	// End of Flag
+	token_eof = -1;
+
+	// Definition Tokens
+	token_define = -2;
+	token_extern = -3;
+
+	// Primary Tokens
+	token_identifier = -4;
+	token_number = -5;
+};
+
+static std::string identifier; 		// Filled if token_identifier -> -4
+static double number_val;		// Filled if token_number -> -5
+
+static int get_token() const {
+	static int last_char = ' ';	// Whitespace
+	
+	// Skip whitespace found in Token
+	while(is_whitespace(last_char)) 
+		last_char = getchar();
+	
+	// If the token is an alpha value i.e character or string
+	if(is_alpha(last_char)) {
+		identifier = last_char;
+		
+		while(is_alpha_num((last_char = getchar())))
+			identifier += last_char;
+		
+		// return token value for each match
+		if(identifier == "define") return token_define;
+
+		if(identifier == "extern") return token_extern;
+		
+		return token_identifier;	
+	}
+	
+	// If the token is of numeric value i.e double or integer
+	if(is_digit(last_char) || last_char == '.') {
+		std::string num_string;
+
+		do {
+			num_string += last_char;
+			last_char = getchar();
+		} while(is_digit(last_char) || last_char == '.');
+
+		return token_number;
+	}
+
+	if(last_char == "//") {
+		do last_char = getchar();
+		while (last_char != EOF && last_char != '\n' && last_char != '\r');
+
+		if(last_char != EOF) return get_token();
+	}
+
+	if(last_char == EOF) return token_eof;
+
+	int this_char = last_char;
+	last_char = getchar();
+
+	return this_char;
+}
---
+++ parser.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+
+using std::unique_ptr;
+
+class ExpressAST {
+
+	public:
+		virtual ~ExpressAST() = default;
+};
+
+// Base class for Numeric values and constants
+class NumberExpression : public ExpressAST {
+
+	protected:
+		double value;
+
+	public:
+		NumberExpression(double val) : value(val) {}
+};
+
+// Base class for expressions that can change
+class VariableExpression : public {
+
+	protected:
+		std::string var_name;
+
+	public:
+		VariableExpression(const std::string & var_n) : var_name(var_n) {}
+};
+
+// Base class for Binary operators
+class BinaryExpression : ExpressAST {
+	
+	protected:
+		char bin_op;
+		unique_ptr<ExpressAST> LHS, RHS;
+
+	public:
+		BinaryExpression(char op, unique_ptr<ExpressAST> LHS, unique_ptr<ExpressAST> RHS)
+			: bin_op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+// Base class for function calls and callers
+class CallExpressionAST(const std::string & caller, std::vector<unique_ptr<ExpressAST>> args) {
+	
+	protected:
+		std::string caller;
+		std::vector<unique_ptr<ExpressAST>> args;
+
+	public:
+		CallExpressionAST(const std::string & caller, std::vector<unique_ptr<ExpressAST>> arguments)
+			: caller(caller), args(std::move(arguments)) {}
+};
+
+// Base class for representing function prototypes i.e signature declarations
+// without the necessary body until linking
+class PrototypeAST {
+	
+	protected:
+		std::string protoype_name;
+		std::vector<std::string> args;
+
+	public:
+		PrototypeAST(const std::string & name, std::vector<std::string> arguments)
+			: prototype_name(name), args(arguments) {}
+
+		const std::string & get_prototype_name() const { return prototype_name; }
+};
+
+// Base class for representing a function definition
+// for an exisiting prototype
+class FunctionAST {
+	
+	protected:
+		unique_ptr<PrototypeAST> prototype;	// i.e add(i8, i8)
+		unique_ptr<ExpressAST> definition;	// i.e { return x + y; }
+	
+	public:
+		FunctionAST(unique_ptr<PrototypeAST> proto, unique_ptr<ExpressAST> def)
+			: prototype(std::move(proto)), definition(std::move(def)) {} 
+};
+
+// a premature test case sample
+
+using std::make_unique;
+
+static auto LHS = make_unique<VariableExpression>("x");
+static auto RHS = make_unique<VariableExpression>("y");
+static auto Result = make_unique<BinaryExpression>('+', std::move(LHS), std::move(RHS));
+
+
